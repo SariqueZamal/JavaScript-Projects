f7fa754909ffc123b98597847465e28ffb56c259
SariqueZamal <sariquezamal0@gmail.com>
2023-01-15 14:16:14 +0530

    calculator

---
+++ Calculator/js/app.js
@@ -0,0 +1,17 @@
+function val(result){
+  form.disp.value = form.disp.value + result;
+}
+
+
+function calculate(){
+  if(form.disp.value == ""){
+    alert("Please Enter numbers")
+  }else{
+    form.disp.value = eval(form.disp.value);
+  }
+}
+
+var btn = form.veql;
+btn.addEventListener("dblclick", function(){
+  form.disp.value = "" //  when we click on equal button
+});
