008002c07768366fd793ab179a153e1434a4c4b2
Sarique Zamal <102463272+SariqueZamal@users.noreply.github.com>
2025-10-11 20:38:39 +0300

    Initial code in script.js

--- Grocery Bud/script.js
+++ Grocery Bud/script.js
@@ -1,1 +1,15 @@
+let input = document.querySelector("input");
+let submit = document.querySelector("button");
+let container = document.querySelector("#container")
 
+
+submit.addEventListener("click",function(){
+    let trimValue=input.value.trim()
+    if(trimValue==""){
+        alert("please enter a value")
+    } 
+    // window.location.reload();
+    container.append(`<div>
+        <p>${trimValue}</p>
+    </div>`)
+});
