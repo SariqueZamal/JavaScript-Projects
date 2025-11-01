f7a4ec4521bf5dd7adec53db2bf9573617e8579c
Sarique Zamal <102463272+SariqueZamal@users.noreply.github.com>
2025-11-01 21:17:21 +0300

    Update script.js

--- Grocery Bud/script.js
+++ Grocery Bud/script.js
@@ -1,15 +1,31 @@
-let input = document.querySelector("input");
-let submit = document.querySelector("button");
-let container = document.querySelector("#container")
+let input = document.querySelector("#input");
+let submit = document.querySelector("#submit");
+let container = document.querySelector(".grocery-list");
+let clearBtn = document.querySelector(".clear")
+let alert = document.querySelector(".alert")
 
+submit.addEventListener("click", function(e) {
+  e.preventDefault();
+  let article = document.createElement("article");
+  article.classList.add("grocery-item");
+  article.innerHTML = `
+  <p class="title">${input.value}</p>
+            <div class="btn-conatiner">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+  `;
+  container.append(article);
+  clearBtn.style.visibility ="visible";
+  setTimeout(() => {
+    alert.innerText = "Item added Successfully."
+  },1000);
 
-submit.addEventListener("click",function(){
-    let trimValue=input.value.trim()
-    if(trimValue==""){
-        alert("please enter a value")
-    } 
-    // window.location.reload();
-    container.append(`<div>
-        <p>${trimValue}</p>
-    </div>`)
+  setTimeout(()=>{
+    alert.remove()
+  },2000)
 });
