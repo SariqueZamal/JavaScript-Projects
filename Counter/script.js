91216f6181f9048a2259163126450acd167e26f8
Sarique Zamal <102463272+SariqueZamal@users.noreply.github.com>
2025-11-02 20:22:02 +0300

    Create script.js

---
+++ Counter/script.js
@@ -0,0 +1,4 @@
+let paragraph = document.querySelector("p");
+let decBtn = document.querySelector("#decrease");
+let incBtn = document.querySelector("#increase");
+let reset = document.querySelector("#reset");
