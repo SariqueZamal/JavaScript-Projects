619808f38ab97f0e05fca00f7e14a2bf538375a0
Sarique Zamal <102463272+SariqueZamal@users.noreply.github.com>
2025-11-02 20:37:07 +0300

    Update script.js

--- Counter/script.js
+++ Counter/script.js
@@ -2,3 +2,23 @@ let paragraph = document.querySelector("p");
 let decBtn = document.querySelector("#decrease");
 let incBtn = document.querySelector("#increase");
 let reset = document.querySelector("#reset");
+
+let count = 0;
+
+// Increase function
+incBtn.addEventListener("click", function () {
+  count++;
+  paragraph.innerText = count;
+});
+
+// reset function
+reset.addEventListener("click", function () {
+  count = 0;
+  paragraph.innerText = count;
+});
+
+// Decrease function
+decBtn.addEventListener("click", function () {
+  count--;
+  paragraph.innerText = count;
+});
