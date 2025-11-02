d3230313362576e649f1f1bf9b1a531010073aad
Sarique Zamal <102463272+SariqueZamal@users.noreply.github.com>
2025-11-02 20:30:42 +0300

    Update index.js

--- Counter/index.js
+++ Counter/index.js
@@ -3,19 +3,19 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js" defer></script>
     <title>Counter App</title>
   </head>
   <body>
     <div class="count">
       <h1>Counter</h1>
-      <p></p>
+      <p>0</p>
     </div>
     <div class="button">
-        <button type="button">Decrease</button>
-        <button type="reset">Reset</button>
-        <button type="button">Increase</button>
+      <button type="button" id="decrease">Decrease</button>
+      <button type="reset" id="reset">Reset</button>
+      <button type="button" id="increase">Increase</button>
     </div>
   </body>
 </html>
