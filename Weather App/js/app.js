e755b47fa215d97d8b620d22a5caae4b033ad727
Sarique Zamal <102463272+SariqueZamal@users.noreply.github.com>
2026-01-20 22:48:30 +0530

    Update app.js

--- Weather App/js/app.js
+++ Weather App/js/app.js
@@ -3,7 +3,6 @@ const form = document.querySelector("form");
 const search = document.querySelector("#search");
 const weather = document.querySelector("#weather");
 
-
 // const API = `https://api.openweathermap.org/data/2.5/weather?
 // q=${city}&appid=${API_KEY}&units=metric`;
 // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
